2cad373755057b23a64bfda5644abaae95473b4f
helcaro <helcaro@publicistgroupe.net>
2022-09-21 13:21:23 -0500

    Guess game  : status -- completed

---
+++ src/js/components/guessNumberGame/stringConstants.js
@@ -0,0 +1,15 @@
+const initialString = 'Start guessing...';
+const noNumberString = 'No Number!';
+const correctString = 'Correct Number!';
+const tooHighString = 'Too High!';
+const tooLowString = 'Too Low!';
+const youLostString = 'Ops, You Lost!';
+
+export {
+  initialString,
+  noNumberString,
+  correctString,
+  tooHighString,
+  tooLowString,
+  youLostString
+};
